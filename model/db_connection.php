83b12c2946c4bb0e9dd906437b7fd468d3dde2c1
James Helms <58826589+jhelms82@users.noreply.github.com>
2021-03-31 14:25:56 -0500

    Update db_connection.php

--- model/db_connection.php
+++ model/db_connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$dsn = 'mysql:host=g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=tcbvo63ef7tkc7sw';
+$dsn = 'mysql:host=	g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=tcbvo63ef7tkc7sw';
 $username = 'vlbfph5v7vzcbm45';
 $password = 'bm19bphgp820e7er';
 
