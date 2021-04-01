c9d4efe42dd253ba2e67746de4e434fcfcb3b660
James Helms <jhelms1014@gmail.com>
2021-04-01 17:35:26 -0500

    commit

--- index.php
+++ index.php
@@ -104,48 +104,10 @@ $result = $query->fetchall();
 
 <?php
 
-echo
-"<table border='2' class='table-primary'>
-    <tr>
-    <th>Year</th>
-    <th>Make</th>
-    <th>Model</th>
-    <th>Type</th>
-    <th>Class</th>
-    <th>Price</th>
 
 
-    </tr>"
 
 ?>
-
-
-
-
-<?php $vehicles = get_vehicles ();
-
-foreach ($vehicles as $vehicle) : ?>
-    <tr>
-        <td><?php echo $vehicle['vehicleYear']; ?></td>
-        <td><?php echo $vehicle['makeName']; ?></td>
-        <td><?php echo $vehicle['vehicleModel']; ?></td>
-        <td><?php echo $vehicle['typeName']; ?></td>
-        <td><?php echo $vehicle['className']; ?></td>
-
-
-        <td class="right"><?php echo $vehicle['vehiclePrice']; ?></td>
-
-    </tr>
-<?php endforeach; ?>
-</table>
-
-
-
-    <?php
-    include'view/footer.php';
-
-
-    ?>
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
