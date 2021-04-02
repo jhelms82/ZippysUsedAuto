3de20ee2e76e3d96e51d9b687600dba3c9701d78
James Helms <jhelms1014@gmail.com>
2021-04-01 23:11:55 -0500

    1

--- index.php
+++ index.php
@@ -44,20 +44,7 @@ $smt->execute();
 $data = $smt->fetchAll();
 
 ?>
-<?php
 
-$sql = "SELECT makeName FROM make";
-$stmt = $db->prepare($sql);
-$stmt->execute();
-
-$smt = $db->prepare('select className From class');
-$smt->execute();
-$data = $smt->fetchAll();
-
-?>
-<header class="list_row list_header">
-    <h1>Vehicle List</h1>
-</header>
 
 
 
